074cd16f95ce7f83e024d08a013baae0251ec3a3
hafet17 <do.crazy192@gmail.com>
2021-08-13 00:14:07 +0700

    implement components alert 1

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -2,6 +2,12 @@
 
 @section('content')
 
+
+@component('components.alert')
+@slot('alert_title') alert-success @endslot
+Lorem ipsum dolor sit amet consectetur adipisicing elit. Sint, quae?
+@endcomponent
+
 <h1>Home Page</h1>
 
 @endsection
