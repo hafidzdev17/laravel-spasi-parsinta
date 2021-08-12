658dcff24ce38b6d6e245e33cc0c0532097d2fb2
hafet17 <do.crazy192@gmail.com>
2021-08-13 00:15:07 +0700

    complete route

--- routes/web.php
+++ routes/web.php
@@ -3,8 +3,10 @@
 use Illuminate\Support\Facades\Route;
 
 
-// Route::get('/', fn () => '<img src="assets/images/image.png"  width="400" height="400"/>'); // php 7.4
+// Route::get('/', fn () => '<img src="assets/images/image.png"  width="400" height="400"/>');
 
-
-Route::get('/', fn () => view('home')); // php 7.4
-Route::get('/profile', fn () => view('profile')); // php 7.4
+// php 7.4
+Route::get('/', fn () => view('home'));
+Route::get('/profile', fn () => view('profile'));
+Route::get('/about', fn () => view('about'));
+Route::get('/contact', fn () => view('contact'));
