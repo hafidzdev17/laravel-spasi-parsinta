12b6bc390fe9329f1e10afd887058f8a0ff0857e
hafet17 <do.crazy192@gmail.com>
2021-08-13 00:28:44 +0700

    implement slot home

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -3,10 +3,17 @@
 @section('content')
 
 
-@component('components.alert')
-@slot('alert_title') alert-success @endslot
-Lorem ipsum dolor sit amet consectetur adipisicing elit. Sint, quae?
-@endcomponent
+<x-alert>
+    <x-slot name="title">
+        alert-success
+    </x-slot>
+    @slot('body')
+    Lorem ipsum dolor sit amet consectetur adipisicing elit. Sint, quae?
+    @endslot
+    @slot('footer')
+    Lorem ipsum dolor sit amet consectetur, adipisicing elit. Velit, adipisci.
+    @endslot
+</x-alert>
 
 <h1>Home Page</h1>
 
