9b6610981da62258380d60d591beb78a300ef19f
hafet17 <do.crazy192@gmail.com>
2021-08-13 23:27:50 +0700

    added slot about

--- resources/views/about.blade.php
+++ resources/views/about.blade.php
@@ -1,3 +1,11 @@
 <x-app-layouts title="About">
-   
+    @slot('style')
+    <style>
+       body {
+          background: indigo;
+          color: azure;
+       }
+    </style>
+@endslot
+<h1>About Page</h1>
 </x-app-layouts>
