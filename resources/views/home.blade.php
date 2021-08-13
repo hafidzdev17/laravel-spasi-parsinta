626fff492eea87f0bd6f641b7975c84c221a3972
hafet17 <do.crazy192@gmail.com>
2021-08-13 20:56:51 +0700

    implement home component

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,20 +1,3 @@
-@extends('layouts.app',['title' => 'Halaman Home'])
-
-@section('content')
-
-
-<x-alert>
-    <x-slot name="title">
-        alert-success
-    </x-slot>
-    @slot('body')
-    Lorem ipsum dolor sit amet consectetur adipisicing elit. Sint, quae?
-    @endslot
-    @slot('footer')
-    Lorem ipsum dolor sit amet consectetur, adipisicing elit. Velit, adipisci.
-    @endslot
-</x-alert>
-
-<h1>Home Page</h1>
-
-@endsection
+<x-app-layouts title="Home Page">
+   <h1>Home </h1>
+</x-app-layouts>
