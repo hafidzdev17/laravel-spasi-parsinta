8d266c3e3791ea5e00d555a0564830e1232a253b
hafet17 <do.crazy192@gmail.com>
2021-09-06 18:35:03 +0700

    modify alert components

--- resources/views/components/alert.blade.php
+++ resources/views/components/alert.blade.php
@@ -1,5 +1,7 @@
-<div class="alert {{ $title }}" role="alert">
-    {{ $body }}
-    <br>
-    {{ $footer }}
+<div class="alert {{ $title ?? '' }} alert-dismissible fade show" role="alert">
+    <button type="button" class="close" data-dismiss="alert" aria-label="Close">
+        <span aria-hidden="true">&times;</span>
+        <span class="sr-only">Close</span>
+    </button>
+    <strong> {{ $body ?? '' }}</strong>
 </div>
