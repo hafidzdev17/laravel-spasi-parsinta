33a309220769d94eb7e6548c0d07dc7fee44c0d6
hafet17 <do.crazy192@gmail.com>
2021-09-06 18:35:57 +0700

    create route upload

--- routes/web.php
+++ routes/web.php
@@ -1,12 +1,12 @@
 <?php
 
-use App\Http\Controllers\{HomeController, ProfileController, ContactController, TaskController};
+use App\Http\Controllers\{HomeController, ProfileController, ContactController, TaskController, UploadController};
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', HomeController::class);
 Route::get('profile', ProfileController::class);
 Route::get('contact', ContactController::class);
-
+Route::post('upload', [UploadController::class, 'store']);
 
 Route::get('task', [TaskController::class, 'index']);
 Route::post('task', [TaskController::class, 'store']);
