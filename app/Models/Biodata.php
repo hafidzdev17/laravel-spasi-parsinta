4675c8e0e87032c5427827c2e88e9bb163e0bda1
hafet17 <do.crazy192@gmail.com>
2021-09-06 18:34:17 +0700

    create biodata model

---
+++ app/Models/Biodata.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Biodata extends Model
+{
+    use HasFactory;
+
+    protected $table = 'biodata';
+
+    protected $fillable = ['nama', 'nim', 'alamat', 'foto'];
+}
