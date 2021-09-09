2fbc69cc4f98fa9e4c1e40e9645cdd0cb473730f
hafet17 <do.crazy192@gmail.com>
2021-09-09 19:24:46 +0700

    implement filter scope homecontroller

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -2,12 +2,17 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Biodata;
 use Illuminate\Http\Request;
 
 class HomeController extends Controller
 {
     public function __invoke()
     {
-        return view('home');
+        return view('home', [
+            'user' => Biodata::latest()
+                ->filter(request(['search']))
+                ->paginate(6)->withQueryString()
+        ]);
     }
 }
