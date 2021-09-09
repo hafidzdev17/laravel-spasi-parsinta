79e2e2592011c40ee675d332b97dbfc5fefd7a22
hafet17 <do.crazy192@gmail.com>
2021-09-09 19:24:21 +0700

    create scopeFIlter biodata model

--- app/Models/Biodata.php
+++ app/Models/Biodata.php
@@ -2,8 +2,10 @@
 
 namespace App\Models;
 
+use GuzzleHttp\Psr7\Request;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use PhpParser\Node\Expr\Cast\Array_;
 
 class Biodata extends Model
 {
@@ -12,4 +14,20 @@ class Biodata extends Model
     protected $table = 'biodata';
 
     protected $fillable = ['nama', 'nim', 'alamat', 'foto'];
+    protected $dates = ['created_at', 'updated_at'];
+
+    public function getSomeDateAttribute($date)
+    {
+        return $date->format('y-m-d');
+    }
+
+    public function scopeFilter($query, array $filter)
+    {
+        $query->when($filter['search'] ?? false, function ($query, $search) {
+            return $query->where(function ($query) use ($search) {
+                $query->where('nama', 'like', '%' . $search . '%')
+                    ->orWhere('nim', 'like', '%' . $search . '%');
+            });
+        });
+    }
 }
