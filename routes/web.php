c98192607e566a203a9ad9f0b8b24ef2bcc25ce0
hafet17 <do.crazy192@gmail.com>
2021-10-19 18:42:06 +0700

    create route web

--- routes/web.php
+++ routes/web.php
@@ -1,12 +1,16 @@
 <?php
 
-use App\Http\Controllers\{HomeController, ProfileController, ContactController, TaskController, UploadController};
+use App\Http\Controllers\{HomeController, ProfileController, ContactController, TaskController, UploadController, AjaxController};
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', HomeController::class);
 Route::get('profile', ProfileController::class);
 Route::get('contact', ContactController::class);
-Route::post('upload', [UploadController::class, 'store']);
+Route::post('upload', [UploadController::class, 'store'])->name('upload');
+
+// ajax
+Route::get('test', [AjaxController::class, 'create']);
+Route::post('contact', [AjaxController::class, 'store']);
 
 Route::get('task', [TaskController::class, 'index']);
 Route::post('task', [TaskController::class, 'store']);
